447749cbe474c250e9bbad0ad52f8cac01b6dc02
Marco Estrada <marco2110estrada@gmail.com>
2021-04-27 23:23:42 -0400

    adding models, connection and server files

---
+++ controllers/index.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const dashboard = require('./dashboardRoutes');
+const homeRoutes = require('./homeRoutes');
+const apiRoutes = require('./api');
+
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboard);
+router.use('/api', apiRoutes);
+
+router.use( (req, res) => {
+    res.send(404).end();
+})
+
+module.exports = router;
---
+++ models/index.js
@@ -0,0 +1,39 @@
+const User = require('./user');
+const Comment = require('./comment');
+const Post = require('./post');
+
+User.hasMany(Post, { 
+    foreign_key: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+User.hasMany(Comment, {
+    foreign_key: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+Comment.belongsTo(Post, {
+    foreign_key: 'post_id',
+    onDelete: 'CASCADE',
+});
+
+Post.hasMany(Comment, {
+    foreign_key: 'post_id',
+    onDelete: 'CASCADE',
+});
+
+Post.belonsToMany(User, { 
+    foreing_key: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+Comment.belongsToMany (User, { 
+    foreign_key: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+module.exports = {
+    User,
+    Comment,
+    Post
+}
